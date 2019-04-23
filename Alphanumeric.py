c93de0c357cdffce1ed9d6c556cb8c4a4a0a8dbc
SHRISTI JALAN <shristij81@gmail.com>
2019-04-23 21:50:19 +0530

    Update Alphanumeric.py

--- Alphanumeric.py
+++ Alphanumeric.py
@@ -1,6 +1,21 @@
+"""
+Given an alphanumeric string S, extract maximum numeric value from that string. All the
+alphabets are in lower case. Take the maximum consecutive digits as a single number.
+Input Format:
+The first line contains the string S.
+Output Format:
+Print the maximum value
+Example:
+Input:
+23dsa43dsa98
+Output:
+98
+
+"""
 import re
 
-input='100klh564abc365bg'
+input='100klh564abc365bg'                       #or you can simply take an input from the user
+
 
 list1=re.findall('\d+',input)
 
