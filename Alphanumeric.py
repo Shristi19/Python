05ab70c5525a02445d946bf321cd6d633b6fa8ae
SHRISTI JALAN <shristij81@gmail.com>
2019-04-23 21:53:15 +0530

    Update Alphanumeric.py

--- Alphanumeric.py
+++ Alphanumeric.py
@@ -16,13 +16,14 @@ import re
 
 input='100klh564abc365bg'                       #or you can simply take an input from the user
 
-
+#Regular expression to find numbers is "\d+". re.findall will find all the patterns matching the argument.
 list1=re.findall('\d+',input)
 
 print(list1)
 
+#Since every number in the list is still a string we need to convert it into integers
 integer_list=list(map(int,list1))
-
+#The list is converted from a list of string to a list of integers
 print(integer_list)
-
+#To print the max of the numbers from the list just use the method max on the list.
 print(max(integer_list))
