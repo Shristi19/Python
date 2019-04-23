f4cec37ee0e29d0de2922176a09ad355b81eddd1
SHRISTI JALAN <shristij81@gmail.com>
2019-04-23 22:06:47 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,3 +1,20 @@
+"""
+Given a string S of lowercase letters, remove consecutive vowels from S. After removing,
+the order of the list should be maintained.
+Input Format:
+Sentence S in a single line
+Output Format:
+Print S after removing consecutive vowels
+Example:
+Input:
+your article is in queue
+Output:
+yor article is in qu
+
+"""
+
+
+
 string='your article is in queue'
 
 def isvowel(c):
